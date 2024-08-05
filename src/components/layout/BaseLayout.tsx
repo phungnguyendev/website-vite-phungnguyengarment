0cadf33cc543fe832986f5052d278713a817d7b0
phungnguyendev <phungnguyengarment.dev@gmail.com>
2024-08-05 09:32:58 +0700

    feat: Scroll to top component

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -1,6 +1,7 @@
 import { Flex } from 'antd'
 import React from 'react'
 import { cn } from '~/utils/helpers'
+import ScrollToTop from '../sky-ui/ScrollToTop'
 
 interface Props {
   onLoading?: (enable: boolean) => void
@@ -11,12 +12,15 @@ interface Props {
 
 const BaseLayout: React.FC<Props> = ({ header, ...props }) => {
   return (
-    <Flex vertical className={cn('w-full gap-[20px] sm:gap-[40px]', props.className)}>
-      {header}
-      <Flex vertical className='w-full gap-[160px]'>
-        {props.children}
+    <>
+      <Flex vertical className={cn('w-full gap-[20px] sm:gap-[40px]', props.className)}>
+        {header}
+        <Flex vertical className='w-full gap-[160px]'>
+          <ScrollToTop />
+          {props.children}
+        </Flex>
       </Flex>
-    </Flex>
+    </>
   )
 }
 
---
+++ src/components/sky-ui/ScrollToTop.tsx
@@ -0,0 +1,17 @@
+import React, { HTMLAttributes, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+export interface ScrollToTopProps extends HTMLAttributes<HTMLDivElement> {}
+
+const ScrollToTop: React.FC<ScrollToTopProps> = ({ ...props }) => {
+  const location = useLocation()
+  useEffect(() => {
+    if (!location.hash) {
+      window.scrollTo(0, 0)
+    }
+  }, [location])
+
+  return <>{props.children}</>
+}
+
+export default ScrollToTop
